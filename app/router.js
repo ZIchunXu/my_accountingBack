235a9c93b3ad20cf60f0f25a311c63d38793f1a7
ZIchunXu <zxu72@my.bcit.ca>
2022-10-01 19:18:12 -0700

    upload router

--- app/router.js
+++ app/router.js
@@ -6,17 +6,32 @@
 module.exports = app => {
   const { router, controller, middleware } = app;
   const _jwt = middleware.jwtErr(app.config.jwt.secret);
+  // register
   router.post('/api/user/register', controller.user.register);
+  // 
   router.post('/api/user/login', controller.user.login);
+  // 
   router.get('/api/user/getuser', _jwt, controller.user.getUserInfor);
+  // 
   router.post('/api/user/editpassword', _jwt, controller.user.editPassword);
+  // 
   router.post('/api/user/editabout', _jwt, controller.user.editUserAbout);
+  // 
   router.post('/api/user/editavatar', _jwt, controller.user.editAvatar);
   router.post('/api/upload',controller.upload.upload);
   router.get('/api/bill/list', _jwt, controller.bill.getBillList);
+  // 
   router.post('/api/bill/add', _jwt, controller.bill.addBill);
+  // 
   router.get('/api/bill/detail', _jwt, controller.bill.getDetail);
+  // 
   router.post('/api/bill/update', _jwt, controller.bill.editBill);
+  // 
   router.post('/api/bill/delete', _jwt, controller.bill.deleteBill);
+  // 
   router.get('/api/bill/data', _jwt, controller.bill.data);
+  // 
+  router.get('/api/type/list', _jwt, controller.type.getTypeList);
+  // 
+  // router.get('/api/type/add', _jwt, controller.type.data);
 };
