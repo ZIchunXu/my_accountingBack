27f47cfca161c2129ff3c1efd198418d2c632709
ZIchunXu <zxu72@my.bcit.ca>
2022-10-04 23:04:39 -0700

    Upload user controller. Put params into data list

--- app/controller/user.js
+++ app/controller/user.js
@@ -7,30 +7,32 @@ const Controller = require('egg').Controller;
 class UserController extends Controller {
     // Get user information
     async getUserInfor() {
-        const {ctx, app} = this;
+        const { ctx, app } = this;
         const token = ctx.request.header.authorization;
         const decode = app.jwt.verify(token, app.config.jwt.secret);
         const info = await ctx.service.user.getUser(decode.username);
         ctx.body = {
             code: 200,
             msg: 'successful',
-            username: info.username,
-            password: info.password,
-            about: info.about,
-            avatar: info.avatar,
+            data: {
+                username: info.username,
+                password: info.password,
+                about: info.about,
+                avatar: info.avatar,
+            }
         };
     }
     // Register account
     async register() {
-        const {ctx} = this;
-        const {username, password} = ctx.request.body;
+        const { ctx } = this;
+        const { username, password } = ctx.request.body;
         if (!username || !password) {
             ctx.body = {
                 code: 500,
                 msg: 'need to fill username and password',
                 data: null,
-              };
-              return;
+            };
+            return;
         }
         //If database has the username, den cant create new user
         const hasUser = await ctx.service.user.getUser(username);
@@ -40,11 +42,11 @@ class UserController extends Controller {
                 code: 500,
                 msg: 'this username already exists',
                 data: null,
-              };
-              return;
+            };
+            return;
         }
         try {
-            const result = await ctx.service.user.register({username, password});
+            const result = await ctx.service.user.register({ username, password });
             ctx.body = {
                 code: 200,
                 msg: 'registration is succesful',
@@ -52,25 +54,25 @@ class UserController extends Controller {
             };
         } catch (error) {
             ctx.body = {
-              code: 500,
-              msg: 'registration failed',
-              data: null,
+                code: 500,
+                msg: 'registration failed',
+                data: null,
             };
         }
     }
 
     //Login
     async login() {
-        const {ctx, app} = this;
-        const {username, password} = ctx.request.body;
+        const { ctx, app } = this;
+        const { username, password } = ctx.request.body;
         const hasUser = await ctx.service.user.getUser(username);
         if (!hasUser) {
             ctx.body = {
                 code: 500,
                 msg: 'Plz create an account first',
                 data: null,
-              };
-              return;
+            };
+            return;
         }
 
         if (hasUser && password !== hasUser.password) {
@@ -98,16 +100,16 @@ class UserController extends Controller {
 
     // Change Password
     async editPassword() {
-        const {ctx, app} = this;
-        const {password = ''} = ctx.request.body;
+        const { ctx, app } = this;
+        const { password = '' } = ctx.request.body;
         try {
             const token = ctx.request.header.authorization;
             const decode = app.jwt.verify(token, app.config.secret);
-            if(!decode){
+            if (!decode) {
                 return;
             }
             const info = await ctx.service.user.getUser(decode.username);
-            const result = await ctx.service.user.editInfor(info.username, {password});
+            const result = await ctx.service.user.editInfor(info.username, { password });
             ctx.body = {
                 code: 200,
                 msg: 'succesful',
@@ -118,27 +120,28 @@ class UserController extends Controller {
                     about: info.about,
                     avatar: info.avatar,
                 },
-              };
-        } catch (err) {console.log('aaa');
+            };
+        } catch (err) {
+            console.log('aaa');
             ctx.body = {
                 code: 500,
                 msg: 'change password fail',
                 data: null,
-              };
+            };
         }
     }
     // Change User About
     async editUserAbout() {
-        const {ctx, app} = this;
-        const {about = ''} = ctx.request.body;
+        const { ctx, app } = this;
+        const { about = '' } = ctx.request.body;
         try {
             const token = ctx.request.header.authorization;
             const decode = app.jwt.verify(token, app.config.secret);
-            if(!decode){
+            if (!decode) {
                 return;
             }
             const info = await ctx.service.user.getUser(decode.username);
-            const result = await ctx.service.user.editInfor(info.username, {about});
+            const result = await ctx.service.user.editInfor(info.username, { about });
             ctx.body = {
                 code: 200,
                 msg: 'succesful',
@@ -149,27 +152,27 @@ class UserController extends Controller {
                     about,
                     avatar: info.avatar,
                 },
-              };
+            };
         } catch (err) {
             ctx.body = {
                 code: 500,
                 msg: 'change user about fail',
                 data: null,
-              };
+            };
         }
     }
     // Change Avatar
     async editAvatar() {
-        const {ctx, app} = this;
-        const {avatar = ''} = ctx.request.body;
+        const { ctx, app } = this;
+        const { avatar = '' } = ctx.request.body;
         try {
             const token = ctx.request.header.authorization;
             const decode = app.jwt.verify(token, app.config.secret);
-            if(!decode){
+            if (!decode) {
                 return;
             }
             const info = await ctx.service.user.getUser(decode.username);
-            const result = await ctx.service.user.editInfor(info.username, {avatar});
+            const result = await ctx.service.user.editInfor(info.username, { avatar });
             ctx.body = {
                 code: 200,
                 msg: 'succesful',
@@ -180,13 +183,13 @@ class UserController extends Controller {
                     about: info.about,
                     avatar,
                 },
-              };
+            };
         } catch (err) {
             ctx.body = {
                 code: 500,
                 msg: 'change user avatar fail',
                 data: null,
-              };
+            };
         }
     }
 }
